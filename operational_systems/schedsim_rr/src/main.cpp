c9a99498e2aa95e5b220443b4f46e174e8531272
Leonardo Korndorfer <leokorndorfer@gmail.com>
2010-05-23 19:43:45 -0300

    updated main algorithm

--- operational_systems/schedsim_rr/src/main.cpp
+++ operational_systems/schedsim_rr/src/main.cpp
@@ -1,39 +1,83 @@
+/*
+ *  main.c
+ *  schedsim_rr
+ *
+ *  Created by Leonardo Korndorfer on 5/15/10.
+ *  Copyright 2010. All rights reserved.
+ *
+ *  this is the main file.
+ *
+ */
+
+
 #include <iostream>
 #include "task_queues.h"
 #include "task.h"
 
+using namespace std;
+
+void usage(void){
+	printf("USAGE:\n");
+	printf("\tcommand -ts [0-10] -tsk file1 [file2 .. filen] \n");
+	
+	exit(1);
+}
+
 int main (int argc, char * const argv[]) {
 	/* local variables definition */
 	
-	int i;                             /* generic counters */
-	//scheduler_class scheduler;         /* scheduler object */
+	int i;                             // generic counters
+	task_class tasks[MAX_PROCESSES];   // tasks list
 	
+	                                   // TODO: declare wainting queue
+					   // TODO: declare ready queue
+					   // TODO: declare finish queue
+					   // TODO: declare executing queue
+
 	
-	
+	/* TODO: check if arguments passed by command-line are ok. If not,
+	         show usage */
+	/* TODO: if arguments ok load the instructions inside tasks */
+		
 	/* 
-	 * verify if the files passed are readble
+	 * verify if the files passed are readble, and create the tasks
 	 */
-	for (i=0; ; i++) {
-		/* gather file name and create a new object to store it's attributes */
+	for (i=0; ; i++) {}
+	
 		
-	}
+	printf("we are scheduling!\n");
 	
-	/* create task array. can create only MAX_PROCESSES simultaneous processes */
-	task_class tasks[MAX_PROCESSES];
-	
-	
-	/*
-	 * create the proccess queues: waiting, ready, executing and finish
-	 */
-	
-	/*
-	 * at this point we start to schedule the processess
-	 */
 	while (true) {
-		printf("we are scheduling!\n");
+		/* keep looping on scheduling */
 		
+		/* TODO: schedule among living tasks */
+		/* TODO: if no task is there, busy wait */
+		/* TODO: give CPU to selected task. start thread */
+		/* TODO: change task state */
+		/* TODO: sleep 1/(time quantum) to wait for process CPU time
+		         to wait or process finishes. decrease the wait time
+		         of tasks on the wait queue. If any task is OK to execute
+		         move it to ready */
+		/* TODO: store last tash PID */
+		/* TODO: change last task state and put the descriptor in some
+		         queue */
+
+		
+		/* process finished or time slice ended. new scheduling */
 	}
 	
     
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+/*EOF*/
